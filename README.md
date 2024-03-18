f4d7ee07c801c75e173f135db67246271661191a
Jeffrey Girard <me@jmgirard.com@>
2024-03-18 18:36:54 -0500

    update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # embedpdf Extension For Quarto
 
-This extension provides support for embedding PDF files into Quarto HTML files (including RevealJS presentations). Each file will be added to your HTML file using the `<object type="application/pdf">` tag with a fallback to the `<embed>` tag (for mobile browsers). Just specify the path of the PDF file; you can also optionally change the displayed width, height, CSS class, and CSS style.
+This extension provides support for embedding PDF files into Quarto HTML files (including RevealJS presentations). Each file will be added to your HTML file using the `<object type="application/pdf">` tag with a fallback to either text or (if supplied) an image with a download link for mobile browsers.
 
 ## Installing
+
+You need to run the following command in your terminal while in the same working directory as your Quarto document. This is often easiest using an RStudio project and the RStudio terminal. Doing so will create a new `_extensions` folder in that directory.
 
 ```bash
 quarto add jmgirard/embedpdf
@@ -18,12 +20,9 @@ To embed a PDF file in your document, use the `{{< pdf URL >}}` shortcode. For e
 ```
 {{< pdf dummy.pdf >}}
 {{< pdf dummy.pdf width=100% height=800 >}}
-{{< pdf dummy.pdf border=1 >}}
-{{< pdf dummy.pdf class=myclass >}}
+{{< pdf dummy.pdf image=dummy.png >}}
 ```
 
-## Example
+## Guide
 
-Here is the source code for a minimal example: [example.qmd](example.qmd).
-
-Here is the rendered version of the example: [example.html](https://jmgirard.github.io/embedpdf/example.html).
+Here is a more in-depth guide with rendered examples: [example.html](https://jmgirard.github.io/embedpdf/example.html).
--- _extensions/embedpdf/embedpdf.lua
+++ _extensions/embedpdf/embedpdf.lua
@@ -1,11 +1,15 @@
 function pdf(args, kwargs)
   local data = pandoc.utils.stringify(args[1]) or pandoc.utils.stringify(kwargs['file'])
-  local image = pandoc.utils.stringify(kwargs['image'])
   local width = pandoc.utils.stringify(kwargs['width'])
   local height = pandoc.utils.stringify(kwargs['height'])
-  local class = pandoc.utils.stringify(kwargs['class'])
   local border = pandoc.utils.stringify(kwargs['border'])
-  local force = pandoc.utils.stringify(kwargs['force_image'])
+  local class = pandoc.utils.stringify(kwargs['class'])
+  local image = pandoc.utils.stringify(kwargs['image'])
+  local image_force = pandoc.utils.stringify(kwargs['image_force'])
+  local image_width = pandoc.utils.stringify(kwargs['image_width'])
+  local image_height = pandoc.utils.stringify(kwargs['image_height'])
+  local image_border = pandoc.utils.stringify(kwargs['image_border'])
+  local image_class = pandoc.utils.stringify(kwargs['image_class'])
   
   if width ~= '' then
     width = 'width="' .. width .. '" '
@@ -15,21 +19,37 @@ function pdf(args, kwargs)
     height = 'height="' .. height .. '" '
   end
   
-  if class ~= '' then
-    class = 'class="' .. class .. '" '
-  end
-  
   if border ~= '' then
     border = 'border="' .. border .. '" '
   end
   
+  if class ~= '' then
+    class = 'class="' .. class .. '" '
+  end
+  
+  if image_width ~= '' then
+    image_width = 'width="' .. image_width .. '" '
+  end
+  
+  if image_height ~= '' then
+    image_height = 'height="' .. image_height .. '" '
+  end
+  
+  if image_border ~= '' then
+    image_border = 'border="' .. image_border .. '" '
+  end
+  
+  if image_class ~= '' then
+    image_class = 'class="' .. image_class .. '" '
+  end
+  
   -- detect html
   if quarto.doc.isFormat("html:js") then
-    if force == 'TRUE' then
-      return pandoc.RawInline('html', '<a href="' .. data .. '" download><img src="' .. image .. '" ' .. width .. height .. class .. border .. ' /></a>')
+    if image_force == 'TRUE' then
+      return pandoc.RawInline('html', '<a href="' .. data .. '" download><img src="' .. image .. '" ' .. image_width .. image_height .. image_class .. image_border .. ' /></a>')
     end
     if image ~= '' then
-      return pandoc.RawInline('html', '<object data="' .. data .. '" type="application/pdf"' .. width .. height .. class .. border .. '><a href="' .. data .. '" download><img src="' .. image .. '" ' .. width .. height .. class .. border .. ' /></a></object>')
+      return pandoc.RawInline('html', '<object data="' .. data .. '" type="application/pdf"' .. width .. height .. class .. border .. '><a href="' .. data .. '" download><img src="' .. image .. '" ' .. image_width .. image_height .. image_class .. image_border .. ' /></a></object>')
     else
       return pandoc.RawInline('html', '<object data="' .. data .. '" type="application/pdf"' .. width .. height .. class .. border .. '><a href="' .. data .. '" download>Download PDF file.</a></object>')
     end
